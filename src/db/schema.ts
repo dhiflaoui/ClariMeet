274a00c2eef265fd32ac7b38091898a8ee61c11b
dhiflaouik <belgacem.dhiflaoui@gmail.com>
2025-08-02 18:10:30 +0100

    feat: integrate Drizzle ORM with Neon database and add database configuration

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+// import { relations } from "drizzle-orm";
+import { pgTable, timestamp, integer, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
