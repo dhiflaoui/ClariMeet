4a0c8824862cad4264fe107542b559e7f2d28b83
dhiflaouik <belgacem.dhiflaoui@gmail.com>
2025-08-20 10:22:41 +0100

    feat: integrate TRPC and React Query for improved data fetching and state management.

--- src/components/dashboard/Home-view.tsx
+++ src/components/dashboard/Home-view.tsx
@@ -2,15 +2,26 @@
 import { authClient } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 const HomeView = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(
+    trpc.hello.queryOptions({
+      text: `belga!`,
+    })
+  );
   const router = useRouter();
   const { data: session } = authClient.useSession();
   if (!session) {
     return <div>Please sign in to access the dashboard.</div>;
   }
+
   return (
     <div>
       <h1>Welcome, {session.user.name}!</h1>
+      <p>TRPC says: {data?.greeting}</p>
       <p>This is your dashboard.</p>
       <Button
         onClick={() =>
